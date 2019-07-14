689e925cdfbade90dbabb0ceac6b8b335fa1bcd2
ccasbre27@hotmail.com <ccasbre27@hotmail.com>
2019-07-14 12:27:49 -0600

    Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+// importar rutas
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
